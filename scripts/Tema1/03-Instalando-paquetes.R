bed9ee15a3eaa4ddbbef3da17fde5611b321f1d1
Rich Nieto <richo.nieto@gmail.com>
2024-05-14 00:39:45 -0600

    Creación de carpeta script y primer documento en markdown

---
+++ scripts/Tema1/03-Instalando-paquetes.R
@@ -0,0 +1,37 @@
+#Para instalar algún paquete de nuestro interes utilizamos el comando
+# install.packages("nombre_libreria", dep = TRUE)
+install.packages("tidyverse", dep = TRUE)
+library(tidyverse)
+
+# El paquete o libreria magic crea cuadrados magicos
+install.packages("magic", dep = TRUE)
+library(magic)
+magic(3)
+
+# Si queremos saber que paquetes tenemos instalados, utilizamos el siguiente comando
+# installed.packages()
+installed.packages()
+
+# Se puede utilizar r como una calculadora, para eso podemos escribir las siguientes expresiones 
+# directmente en la consola
+
+# 12+3
+# 3*(2+1 esta expresión regreserá un erro ya que se debe poner completa con todo y paréntesis
+# 9*25+3) nuevamente dara un error hasta que se complete la expresión 
+
+# Se pueden ejecutar varias operaciones en una sola línea, esto se hace poniendo ; después de cada 
+# operación, por ejemplo : 1+2; 2+3; 4+5 
+
+# Las operaciones en r son las conocidas para todos los números reales, es decir:
+
+# +  SUMA
+# -  RESTA
+# /  DIVISIÓN
+# *  MULTIPLICACIÓN
+# %/% DIVISIÓN ENTERA
+# %% MÓDULO
+
+
+
+
+
